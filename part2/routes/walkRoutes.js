5fc95aacb6f6c3da18400648c2e735db65715690
Aini1011 <a1951750@adelaide.edu.au>
2025-06-21 05:48:22 +0000

    [automatic commit] 2025-06-21 05:48:22 UTC 175.35.83.149

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -59,15 +59,4 @@ router.post('/:id/apply', async (req, res) => {
   }
 });
 
-// POST logout
-router.post('/logout', (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      return res.status(500).json({ error: 'Logout failed' });
-    }
-    res.clearCookie('connect.sid');
-    res.json({ message: 'Logout successful' });
-  });
-});
-
 module.exports = router;
