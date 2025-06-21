e7968d12ec744b1de58fc9218218f2ea1e34b2f5
Aini1011 <a1951750@adelaide.edu.au>
2025-06-21 07:57:57 +0000

    [automatic commit] 2025-06-21 07:57:57 UTC 175.35.83.149

--- part2/app.js
+++ part2/app.js
@@ -22,6 +22,8 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/users', require('./routes/userRoutes'));
+
 
 // Export the app instead of listening here
 module.exports = app;
