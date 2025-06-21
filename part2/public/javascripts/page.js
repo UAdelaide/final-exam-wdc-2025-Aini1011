9a65754b5e1add8906b13185247fbf9e4b2c9207
Aini1011 <a1951750@adelaide.edu.au>
2025-06-21 04:13:33 +0000

    [automatic commit] 2025-06-21 04:13:33 UTC 175.35.83.149

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -187,9 +187,15 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome "+this.responseText);
+            const result = JSON.parse(this.responseText);
+            alert("Welcome " + result.user.username);
 
-
+            // Jump to different pages according to user roles
+            if (result.user.role === 'owner') {
+                window.location.href = 'owner-dashboard.html';
+            } else if (result.user.role === 'walker') {
+                window.location.href = 'walker-dashboard.html';
+            }
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
         }
