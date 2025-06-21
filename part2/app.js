0461df95135cb416d2387d2b503f55137cc01cf1
Aini1011 <a1951750@adelaide.edu.au>
2025-06-21 03:55:04 +0000

    [automatic commit] 2025-06-21 03:55:04 UTC 175.35.83.149

--- part2/app.js
+++ part2/app.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+// Session middleware setup
+app.use(session({
+    secret: 'dogwalk-secret-key',
+    resave: false,
+    saveUninitialized: true
+  }));
+  
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
