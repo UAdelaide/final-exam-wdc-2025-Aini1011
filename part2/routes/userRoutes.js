57dc503b296d3e7e020f6b2733c15ce0d1044191
Aini1011 <a1951750@adelaide.edu.au>
2025-06-21 03:06:58 +0000

    [automatic commit] 2025-06-21 03:06:58 UTC 175.35.83.149

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,7 +48,8 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-    //
+
+    //Save User to 
     req.session.user = {
       id: row[0].user_id,
       username: row[0].username,
